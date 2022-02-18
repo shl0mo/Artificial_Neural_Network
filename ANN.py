f26e269ec684c9a057119ad18a7f996f081735cb
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-02-17 21:36:18 -0400

    Feature scaling

--- ANN.py
+++ ANN.py
@@ -15,22 +15,28 @@ import numpy as np
 import tensorflow as tf
 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 dataset = pd.read_csv('Churn_Modelling.csv')
 x = dataset.iloc[:, 3:-1]
 y = dataset.iloc[:, -1]
 
-#Encoding "Gender"
+# Encoding "Gender"
 le = LabelEncoder()
 x.iloc[:, 2] = le.fit_transform(x.iloc[:, 2])
-x
 
-#Enconding "Geography"
+# Enconding "Geography"
 x.iloc[:, 1] = le.fit_transform(x[["Geography"]])
-x
 # As a result, we have:
 # France - 0
 # Germay - 1
 # Spain - 2
 
-_validate_remainder(x)
+# Define training and test sets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
